c736e252f9291ddb0306e533ce55bf96ea1d788c
Victor Prindyn <victorprindyn.ua@gmail.com>
2022-08-04 20:08:13 +0300

    added contacts section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { HashRouter, Route } from "react-router-dom"
 import HomePage from "./components/HomePage/HomePage"
 import Posts from "./components/Posts/Posts"
 import Photos from "./components/Photos/Photos"
-import Contacts from "./components/Contacts/Contacts"
+import ContactsMenu from "./components/ContactsMenu/ContactsMenu"
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Route path="/" exact component={HomePage} />
         <Route path="/posts" component={Posts} />
         <Route path="/photos" component={Photos} />
-        <Route path="/contacts" component={Contacts} />
+        <Route path="/contacts" component={ContactsMenu} />
       </HashRouter>
     </div>
   )
