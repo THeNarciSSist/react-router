3a3f69ec6b33c6ea863053fc1df8a632f251909a
Victor Prindyn <victorprindyn.ua@gmail.com>
2022-08-04 10:22:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Header from "./components/Header/Header"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import HomePage from "./components/HomePage/HomePage"
+import Posts from "./components/Posts/Posts"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/photos" component={Posts} />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
